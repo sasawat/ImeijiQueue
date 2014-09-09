44f3e6f9ce243c191d30e3c5dbaf9509d86eb0b2
Izumi Konata <sasawat@outlook.com>
2014-09-09 12:56:40 -0400

    Created interface for sourcing images

---
+++ ImeijiQueue/ISauce.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImeijiQueue
+{
+    interface ISauce
+    {
+        public static String lookup(String url);
+    }
+}
