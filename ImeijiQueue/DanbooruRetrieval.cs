1a2fd7b30e3c621ec24a5df98f85cb04b1bf185c
Izumi Konata <sasawat@outlook.com>
2014-09-09 20:40:49 -0400

    Started work on retrieving from Danbooru. Created DanbooruRetrieval and IBooruRetrieval interface to aid the adding of additional *booru support.

---
+++ ImeijiQueue/DanbooruRetrieval.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImeijiQueue
+{
+    class DanbooruRetrieval : IBooruRetrieval
+    {
+        public DanbooruRetrieval(String url)
+        {
+
+        }
+
+        public byte[] IBooruRetrieval.getImage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string IBooruRetrieval.getSauceURL()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getTitle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ImeijiQueue/IBooruRetrieval.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImeijiQueue
+{
+    interface IBooruRetrieval
+    {
+
+        public byte[] getImage();
+
+        String getSauceURL();
+
+        String getTitle();
+
+    }
+}
