37484c4dccff749ad7feebfbe3bae4bd0035385e
Izumi Konata <sasawat@outlook.com>
2014-09-09 23:41:25 -0400

    Wrote stuff to get images from Danbooru

--- ImeijiQueue/DanbooruRetrieval.cs
+++ ImeijiQueue/DanbooruRetrieval.cs
@@ -3,29 +3,121 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DashBuddy.API;
+using System.Net;
+using System.IO;
 
 namespace ImeijiQueue
 {
     class DanbooruRetrieval : IBooruRetrieval
     {
+        byte[] Image;
+        String SauceURL;
+        String Title;
+
         public DanbooruRetrieval(String url)
         {
+            //Get the image URL
+            String imageURL = retrieveImageURL(url);
 
+            //Get the image
+            Image = retrieveImage(imageURL);
+
+            //Get the source
+            ISauce sriracha
+            try
+            {
+                sriracha = new SauceNao(imageURL);
+            }
+            catch(Exception sEx)
+            {
+                throw new BooruRetrievalFailedException(BooruRetrievalFailedException.errCode.SauceFailed, sEx);
+            }
+
+            //Get the title and sauce url from the ISauce
+            Title = sriracha.getTitle();
+            SauceURL = sriracha.getSauceURL();
+
+        }
+
+        private String retrieveImageURL(String url)
+        {
+            String page;
+            //Retrieve the page
+            try
+            {
+                page = OAuth.PostData(url, "GET", "", null);
+            }
+            catch (Exception sEx)
+            {
+                throw new BooruRetrievalFailedException(BooruRetrievalFailedException.errCode.NetworkError, sEx);
+            }
+
+            //Find the image url
+            int startIndex;
+            int len;
+
+            //Find the tag for the main image
+            if((startIndex = page.IndexOf("id=\"image\"")) == -1 )
+            {
+                throw new BooruRetrievalFailedException(BooruRetrievalFailedException.errCode.ParseFailed);
+            }
+
+            //Extract the url
+            if((startIndex = page.IndexOf("src=\"", startIndex)) == -1)
+            {
+                throw new BooruRetrievalFailedException(BooruRetrievalFailedException.errCode.ParseFailed);
+            }
+            startIndex += 5;
+
+            len = page.IndexOf("\"", startIndex) - startIndex;
+            if(len <= 0)
+            {
+                throw new BooruRetrievalFailedException(BooruRetrievalFailedException.errCode.ParseFailed);
+            }
+
+            //URL for danbooru is like relative and shit so we have to put in the full url
+            return "http://danbooru.donmai.us" + page.Substring(startIndex, len);
+        }
+
+        private byte[] retrieveImage(String imageURL)
+        {
+            //Make the request
+            WebResponse response;
+            try
+            {
+                response = OAuth.PostDataResponse(imageURL, "GET", "", null);
+            }catch(Exception sEx)
+            {
+                throw new BooruRetrievalFailedException(BooruRetrievalFailedException.errCode.NetworkError, sEx);
+            }
+
+            //Fish the image out of the response
+            Stream river = response.GetResponseStream();
+            MemoryStream memorylane = new MemoryStream();
+            System.Drawing.Image img = System.Drawing.Image.FromStream(river);
+            response.Close();
+
+            //Make sure it's a fucking jpeg
+            img.Save(memorylane, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            //Return it
+            return memorylane.ToArray();
         }
 
         public byte[] IBooruRetrieval.getImage()
         {
-            throw new NotImplementedException();
+            return Image;
         }
 
         public string IBooruRetrieval.getSauceURL()
         {
-            throw new NotImplementedException();
+            return SauceURL;
         }
 
         public string getTitle()
         {
-            throw new NotImplementedException();
+            return Title;
         }
     }
 }
--- ImeijiQueue/IBooruRetrieval.cs
+++ ImeijiQueue/IBooruRetrieval.cs
@@ -16,4 +16,29 @@ namespace ImeijiQueue
         String getTitle();
 
     }
+
+    [Serializable]
+    public class BooruRetrievalFailedException : Exception
+    {
+        public errCode error {get; private set;}
+        public enum errCode {NetworkError, ParseFailed, SauceFailed}
+
+        public BooruRetrievalFailedException() { }
+        public BooruRetrievalFailedException(errCode errorcode)
+        {
+            error = errorcode;
+        }
+
+        public BooruRetrievalFailedException(errCode errorcode, Exception inner) : base("", inner)
+        {
+            error = errorcode;
+        }
+        public BooruRetrievalFailedException() { }
+        public BooruRetrievalFailedException(string message) : base(message) { }
+        public BooruRetrievalFailedException(string message, Exception inner) : base(message, inner) { }
+        protected BooruRetrievalFailedException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
 }
