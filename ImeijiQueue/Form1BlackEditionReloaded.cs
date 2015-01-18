77d239ef44bd9f1bbb22f90a578cc397f5fc0fe5
Izumi Konata <sasawat@outlook.com>
2015-01-18 01:46:25 -0500

    idk

---
+++ ImeijiQueue/Form1BlackEditionReloaded.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImeijiQueue
+{
+    public partial class Form1BlackEditionReloaded : Form
+    {
+        public Form1BlackEditionReloaded()
+        {
+            InitializeComponent();
+        }
+    }
+}
